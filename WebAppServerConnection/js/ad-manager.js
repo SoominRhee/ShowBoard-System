81bd4c368778beb4256957efc39b7700f283e5c6
Soomin Rhee <dltnals@ajou.ac.kr>
2025-04-10 16:14:50 +0900

    refactor: remove dummy nodes and always preserve expand button

--- WebAppServerConnection/js/ad-manager.js
+++ WebAppServerConnection/js/ad-manager.js
@@ -31,40 +31,33 @@
         }
 
 
-        if ($nested.children(".dummy").length > 0) {
-            const dn = $(this).data("dn");
+        const dn = $(this).data("dn");
 
-            $.ajax({
-                url: "../AD/GetChildNodes",
-                type: "GET",
-                data: { dn: dn },
-                dataType: "json",
-                success: function (res) {
-                    //alert("자식 노드 불러옴");
+        $.ajax({
+            url: "../AD/GetChildNodes",
+            type: "GET",
+            data: { dn: dn },
+            dataType: "json",
+            success: function (res) {
+                //alert("자식 노드 불러옴");
 
-                    $nested.empty();
+                $nested.empty();
 
-                    if (res.length > 0) {
-                        res.forEach(child => {
-                            const html = `<li>
-                                        <span class="tree-toggle caret" data-dn="${child.DistinguishedName}" data-type="${child.SchemaClassName}"></span>
-                                        <span class="node-label" data-dn="${child.DistinguishedName}" data-type="${child.SchemaClassName}">${child.Name}</span>
-                                        <ul class="nested">
-                                            <li class = "dummy"></li>
-                                        </ul>
-                                    </li>`
-                            $nested.append(html);
-                        });
-                    } else {
-                        $nested.remove();
-                        $li.find(".tree-toggle").removeClass("caret");
-                    }
-                },
-                error: function () {
-                    alert("자식 노드 불러오기 실패");
-                }
-            })
-        }
+                res.forEach(child => {
+                    const html = `<li>
+                                <span class="tree-toggle caret" data-dn="${child.DistinguishedName}" data-type="${child.SchemaClassName}"></span>
+                                <span class="node-label" data-dn="${child.DistinguishedName}" data-type="${child.SchemaClassName}">${child.Name}</span>
+                                <ul class="nested">
+                                </ul>
+                            </li>`
+                    $nested.append(html);
+                });
+            },
+            error: function () {
+                alert("자식 노드 불러오기 실패");
+            }
+        })
+        
         $nested.toggleClass("active");
         $(this).toggleClass("caret-down");
 
@@ -101,15 +94,17 @@
 
 
     $(document).on("dblclick", ".main-table-body tr", function () {
+        $
         const dn = $(this).data("dn");
-        alert("상세 정보 가져오기")
+        //alert("상세 정보 가져오기")
         $.ajax({
             url: "../AD/GetDetails",
             type: "GET",
             data: { dn: dn },
             dataType: "json",
             success: function (res) {
-                alert("상세 정보 가져오기 성공")
+                //alert("상세 정보 가져오기 성공")
+                $("#detail-box .detail-content").html(`<span class="close-btn">&times;</span>`);
 
                 let html = "";
 
@@ -155,7 +150,6 @@ function loadADTree() {
                             <span class="tree-toggle caret" data-dn="${n.DistinguishedName}" data-type="${n.SchemaClassName}"></span>
                             <span class="node-label" data-dn="${n.DistinguishedName}" data-type="${n.SchemaClassName}">${n.Name}</span>
                             <ul class="nested">
-                                <li class="dummy"></li>
                             </ul>
                         </li>`
             });
